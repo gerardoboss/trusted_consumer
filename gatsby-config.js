700db766a59a1193c4ac0aceb3994b13acc10339
Gerardo Jaramillo <gerardo.jaramillo@gmail.com>
2019-09-20 18:05:21 -0600

    added worksapce create header for trusted

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    "gatsby-plugin-sass",
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -21,14 +22,11 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
+        background_color: `#000`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/gatsby-icon.png`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
--- src/components/header.js
+++ src/components/header.js
@@ -1,34 +1,33 @@
-import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import { Navbar, Container, Row, Col } from "react-bootstrap"
+import "./header.scss"
+import logo from "../images/trusted_consumer_logo.png"
+console.log(logo)
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
+  <Container className="header" fluid="true">
+    <Row>
+      <Col>
+        <Container>
+          <Row>
+            <Col>
+              <Navbar>
+                <Navbar.Brand href="#home">
+                  <img
+                    src={logo}
+                    width="265"
+                    height="62"
+                    alt="Trusted Consumer Logo"
+                  />
+                </Navbar.Brand>
+              </Navbar>
+            </Col>
+          </Row>
+        </Container>
+      </Col>
+    </Row>
+  </Container>
 )
 
 Header.propTypes = {
