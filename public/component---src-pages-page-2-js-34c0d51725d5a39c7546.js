936f1c09bc8e976ec458860740d2ad9ae38bd824
Gerardo Jaramillo <gerardo.jaramillo@gmail.com>
2019-11-21 18:06:52 -0600

    added public

---
+++ public/component---src-pages-page-2-js-34c0d51725d5a39c7546.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{235:function(e,t,a){"use strict";a.r(t);var n=a(0),o=a.n(n),l=a(233),c=a(232),r=a(231);t.default=function(){return o.a.createElement(c.a,null,o.a.createElement(r.a,{title:"Page two"}),o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(l.a,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-34c0d51725d5a39c7546.js.map
---
+++ public/styles-f085d7ab8a6bab37e7bf.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{168:function(n,w,o){}}]);
+//# sourceMappingURL=styles-f085d7ab8a6bab37e7bf.js.map
